95f3c2200e1c80054977b1460d6cd6ce37af199e
Batname <dadubinin@gmail.com>
2017-07-22 14:00:15 +0200

    add umg input

--- Source/FacebookArena/GameSettingsWidget.cpp
+++ Source/FacebookArena/GameSettingsWidget.cpp
@@ -3,6 +3,16 @@
 
 #include "GameSettingsWidget.h"
 
+#include "GameMode_FA.h"
+#include "PlayerController_FA.h"
+
+
+void UGameSettingsWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	GameMode_FA = AGameMode_FA::StaticClass()->GetDefaultObject<AGameMode_FA>();
+}
 
 
 
--- Source/FacebookArena/GameSettingsWidget.h
+++ Source/FacebookArena/GameSettingsWidget.h
@@ -14,7 +14,17 @@ class FACEBOOKARENA_API UGameSettingsWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
-	
+protected:
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString PlayerFacebookID;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString OponentFacebookID;
+private:
+	/** Set reference to game mode */
+	class AGameMode_FA* GameMode_FA;
 	
 	
 };
